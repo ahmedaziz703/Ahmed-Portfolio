00e90feb1d58c2fa7e6847c4aeb31f62c578779b
Ahmed Al-ragas <ahmedazizz703@gmail.com>
2025-06-29 23:46:11 +0300

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6Lf-CXIrAAAAAC67y75NtIX4QMoRevJkZK9NpLzk"
           onChange={(e) => {
             setIsHuman(true)
           }}
